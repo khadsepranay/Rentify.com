75212e006baaf54831bf332ece9eee1a0b70b0e0
vinayak hiwase <vinayakhiwse8@gmail.com>
2022-11-09 23:00:38 +0530

    first-day

---
+++ rent_mojo/src/vh-redux/product/actiontype.js
@@ -0,0 +1,6 @@
+export const GET_PRODUCTS_LOADING = "products/loading";
+export const GET_PRODUCTS_SUCCESS = "products/success";
+export const GET_PRODUCTS_ERROR = "products/error";
+
+// Reset
+//export const RESET_PRODUCTS = "products/reset";
